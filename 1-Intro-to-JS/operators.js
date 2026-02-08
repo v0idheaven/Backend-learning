cbf27210ca39f4d505947a7c180307834f6ab9e7
v0idheaven <dahiyavarun2007@gmail.com>
2026-02-08 17:23:58 +0530

    operators

---
+++ 1-Intro-to-JS/operators.js
@@ -0,0 +1,33 @@
+// arithematic operators
+console.log(10 + 2) // addition
+console.log(10 - 2) // subtraction
+console.log(10 * 2) // multiplication
+console.log(10 / 2) // division
+console.log(10 % 3) // reminder
+console.log(10 ** 2) // exponentiation
+
+// Note if we want to have floor division
+// 1 way -> parseInt(5/2)
+// 2 way -> Math.floor(5/2)
+
+// assignment operators
+let x = 10; // assigning the value 10 to the variable x
+x += 2; // x = x + 2
+x -= 2; // x = x - 2
+x *= 2; // x = x * 2
+x /= 2; // x = x / 2
+x %= 3; // x = x % 3
+x **= 2; // x = x ** 2
+
+// logical operators
+console.log((10 > 5) && (7 < 6));
+console.log((10 > 5) || (7 < 6));
+console.log(4 && 0); // 0
+console.log(4 || 0); // 4
+console.log(0 && 4); // 0
+console.log(0 || 4); // 4
+console.log(-4 || 5); // -4
+console.log(3 && 4); // 4
+console.log(3 || 4); // 3
+console.log(-0 && 9); // -0
+console.log(-0 || 9); // 9
