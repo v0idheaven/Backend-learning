acaaddb4a97c14a59094b721c5ad5f92b2e1ad12
v0idheaven <dahiyavarun2007@gmail.com>
2026-02-08 16:56:18 +0530

    typesof in js

---
+++ 1-Intro-to-JS/typeof.js
@@ -0,0 +1,7 @@
+console.log(typeof(12));
+console.log(typeof("abcd"));
+console.log(typeof(-0));
+console.log(typeof(undefined));
+console.log(typeof(null)); // corner case
+console.log(typeof( {"varun": 12} ));
+console.log(typeof(NaN));
