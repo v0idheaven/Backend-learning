f0ff4fd18d22e01996c4a2c18816086039cf4905
v0idheaven <dahiyavarun2007@gmail.com>
2026-02-09 14:35:48 +0530

    operators complete

---
+++ 2-introduction-to-JS/operators.js
@@ -0,0 +1,11 @@
+// bitwise operators
+console.log(5 & 3);
+console.log(8 | 1);
+console.log(3 ^ 3);
+console.log(5 << 2);
+console.log(5 >> 1); // 101 -> 10
+
+// string concatenation
+console.log("Varun" + " " +"Yadav");
+
+console.log(5 + 5 + "5" + 5 + 5);
