71a2e80a9d6c314543b0af3a55d5e4ea632ee341
v0idheaven <dahiyavarun2007@gmail.com>
2026-02-10 18:00:03 +0530

    switch case

---
+++ 2-introduction-to-JS/switchdemo.js
@@ -0,0 +1,29 @@
+let exp = "+";
+let a = 10;
+let b = 3;
+switch(exp){
+    case"+":
+        console.log(a + b);
+        break;
+    case"-":
+        console.log(a-b);
+        break;
+    case"*":
+        console.log(a*b);
+        break;
+    case"/":
+        console.log(a/b);
+        break;
+    default:
+        console.log("Invalid input");
+}
+
+let val = "10";
+switch(val) {
+    case 10:
+        console.log("Yes");
+        break;
+    default:
+        console.log("No");
+        break;
+}
