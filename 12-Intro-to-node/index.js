61ba3a92412ff077fa17a74153ae646e0d0b5ae4
v0idheaven <dahiyavarun2007@gmail.com>
2026-03-02 23:50:49 +0530

    index.js

---
+++ 12-Intro-to-node/index.js
@@ -0,0 +1,10 @@
+const searching = require('./searching.js')
+// import searching from './searching.js'
+console.log("Start");
+let arr = [5,4,2,7,1,3,4,5,6];
+let x = 6;
+console.log(searching)
+
+const z = await Promise.resolve(1);
+
+// console.log(searching.linear(arr, x));
