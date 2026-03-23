f24d021835a3d154c0f899d12f34c5a19ddaf284
v0idheaven <dahiyavarun2007@gmail.com>
2026-03-23 16:43:17 +0530

    all setup configured

--- Chai aur Backend/Day 03/src/index.js
+++ Chai aur Backend/Day 03/src/index.js
@@ -2,6 +2,8 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import express from "express";
+import { app } from "./app.js";
 
 dotenv.config()
 
