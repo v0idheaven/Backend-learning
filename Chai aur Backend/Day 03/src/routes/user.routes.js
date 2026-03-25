32234740b3b9e1288dd47e37106f8687534b8306
v0idheaven <dahiyavarun2007@gmail.com>
2026-03-26 04:03:44 +0530

    add user account management features: change password, get current user, update account details, and update user images

--- Chai aur Backend/Day 03/src/routes/user.routes.js
+++ Chai aur Backend/Day 03/src/routes/user.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import multer from "multer";
 
 const router = Router();
 
