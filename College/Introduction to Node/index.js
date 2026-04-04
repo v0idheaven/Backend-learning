3b9d6e7ad91967495c3148dc87b4f7c8e1d00857
v0idheaven <dahiyavarun2007@gmail.com>
2026-04-04 23:32:53 +0530

    feat: Add file handling examples and create directory structure in Node.js

--- College/Introduction to Node/index.js
+++ College/Introduction to Node/index.js
@@ -1,6 +1,5 @@
 import fs from "fs";
-
-
+import path from "path";
 
 // reading
 
@@ -16,7 +15,6 @@ import fs from "fs";
 //         console.log('File does not exist');
 //     }
 // }
-
 
 // function customReadFile() {
 //   if (fs.existsSync("text.txt")) {
@@ -41,23 +39,95 @@ import fs from "fs";
 
 // writing
 
-
-fs.writeFile("newFile.txt", "this is the first file for testing.", "utf8", (err) => {
-  if (err) console.error(err);
-});
+// fs.writeFile("newFile.txt", "this is the first file for testing.", "utf8", (err) => {
+//   if (err) console.error(err);
+// });
 
 // fs.appendFileSync("newFile.txt", "\nthis is the second line.", "utf8");
-fs.appendFile("newFile.txt", "\nthis is the second line.", "utf8", (err) => {
-  if (err) console.error(err);
-});
-
+// fs.appendFile("newFile.txt", "\nthis is the second line.", "utf8", (err) => {
+//   if (err) console.error(err);
+// });
 
 // existance
 // console.log(fs.existsSync("newFile.txt"));
 
-
 // delete
 function deleteFile(path) {
-    fs.unlinkSync(path);
+  fs.unlinkSync(path);
 }
-deleteFile("./assets/newFile.txt");
+
+// deleteFile("./assets/newFile.txt");
+
+// rename
+// fs.rename("newfile.txt", "renamedFile.txt", () => {})
+
+// fs.renameSync("./done/newFile.txt", "./done/renamedFile.txt");
+
+// copy
+// fs.copyFileSync("renamedFile.txt", "./renamedFile2.txt");
+
+// fs.copyFile("renamedFile.txt", "./renamedFile2.txt", () => {});
+
+// move
+// fs.rename("file.txt", "folder/file.txt", (err) => {
+// if (err) throw err;
+// });
+
+// Create Directory
+// fs.mkdir("newFolder", (err) => {
+// if (err) throw err;
+// });
+
+// Delete Directory
+// fs.rmdir("folder", (err) => {});
+
+// function addTextToFile(path, text) {
+//   try {
+//     const data = fs.readFileSync(path, "utf8");
+
+//     fs.appendFileSync(path, text, "utf8");
+
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+// addTextToFile("./done/renamedFile.txt", "\nthis is the third line.");
+
+// Problem: Generate Folder Structure from Object
+const fsTree = {
+  type: "folder",
+  name: "parent",
+  children: [
+    { type: "file", name: "first.txt" },
+    { type: "file", name: "second.txt" },
+    {
+      type: "folder",
+      name: "child",
+      children: [{ type: "file", name: "first.txt" }],
+    },
+  ],
+};
+
+// Solution:
+function createFS(node, basePath) {
+  const currentPath = path.join(basePath, node.name);
+  if (node.type === "folder") {
+    // Create folder
+    fs.mkdir(currentPath, { recursive: true }, (err) => {
+      if (err) throw err;
+      // Process children after folder is created
+      if (node.children) {
+        node.children.forEach((child) => {
+          createFS(child, currentPath);
+        });
+      }
+    });
+  } else if (node.type === "file") {
+    // Create file
+    fs.writeFile(currentPath, "", (err) => {
+      if (err) throw err;
+    });
+  }
+}
+// Run
+createFS(fsTree, process.cwd());
